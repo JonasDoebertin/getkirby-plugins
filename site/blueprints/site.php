03d1e1d141012c3701a6cafd87bcf1dfa466f540
Jonas Döbertin <hello@jd-powered.net>
2014-12-17 21:59:17 +0100

    SEO improvements & favicons. Close #3

--- site/blueprints/site.php
+++ site/blueprints/site.php
@@ -6,12 +6,19 @@ fields:
     title:
         label: Title
         type:  text
+        width:  1/2
+    heading:
+        label: Heading
+        type:  text
+        width:  1/2
     description:
-        label: Description
+        label: Meta Description
         type:  textarea
+        width:  1/2
     copyright:
         label: Copyright
         type:  textarea
+        width:  1/2
     googleverification:
         label: Google Site Verification Code
-        type: text
+        type:  text
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,10 +1,12 @@
 <!doctype html>
 <html class="no-js" lang="en">
     <head>
+
+        <!-- meta -->
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title><?= $site->title() ?></title>
-        <meta name="description" content="">
+        <title><?= $site->title()->html() ?></title>
+        <meta name="description" content="<?= $site->description()->html() ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- check js support -->
@@ -12,12 +14,33 @@
             document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
         </script>
 
-        <!--<link rel="apple-touch-icon" href="apple-touch-icon.png">-->
-        <!-- Place favicon.ico in the root directory -->
-
+        <!-- styles -->
         <?= css('//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600') ?>
-        <link rel="stylesheet" href="assets/css/main.@@1418767497500.css">
+        <?= css('/assets/css/main.@@1418767497500.css') ?>
 
+        <!-- favicons -->
+        <link rel="apple-touch-icon" sizes="57x57" href="<?= url('/assets/images/favicons/apple-touch-icon-57x57.png') ?>">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?= url('/assets/images/favicons/apple-touch-icon-114x114.png') ?>">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?= url('/assets/images/favicons/apple-touch-icon-72x72.png') ?>">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?= url('/assets/images/favicons/apple-touch-icon-144x144.png') ?>">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?= url('/assets/images/favicons/apple-touch-icon-60x60.png') ?>">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?= url('/assets/images/favicons/apple-touch-icon-120x120.png') ?>">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?= url('/assets/images/favicons/apple-touch-icon-76x76.png') ?>">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?= url('/assets/images/favicons/apple-touch-icon-152x152.png') ?>">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?= url('/assets/images/favicons/apple-touch-icon-180x180.png') ?>">
+        <meta name="apple-mobile-web-app-title" content="Kirby Plugins">
+        <link rel="shortcut icon" href="<?= url('/assets/images/favicons/favicon.ico') ?>">
+        <link rel="icon" type="image/png" href="<?= url('/assets/images/favicons/favicon-192x192.png') ?>" sizes="192x192">
+        <link rel="icon" type="image/png" href="<?= url('/assets/images/favicons/favicon-160x160.png') ?>" sizes="160x160">
+        <link rel="icon" type="image/png" href="<?= url('/assets/images/favicons/favicon-96x96.png') ?>" sizes="96x96">
+        <link rel="icon" type="image/png" href="<?= url('/assets/images/favicons/favicon-16x16.png') ?>" sizes="16x16">
+        <link rel="icon" type="image/png" href="<?= url('/assets/images/favicons/favicon-32x32.png') ?>" sizes="32x32">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="msapplication-TileImage" content="<?= url('/assets/images/favicons/mstile-144x144.png') ?>">
+        <meta name="msapplication-config" content="<?= url('/assets/images/favicons/browserconfig.xml') ?>">
+        <meta name="application-name" content="Kirby Plugins">
+
+        <!-- misc -->
         <meta name="google-site-verification" content="<?= $site->googleverification() ?>" />
     </head>
     <body class="template--<?= $page->intendedTemplate() ?>">
@@ -29,7 +52,7 @@
                     <img class="logo" src="assets/images/logo.png" width="65" height="65" alt="Plugins &amp; Extensions for Kirby 2">
                 </div>
 
-                <h1 class="header__title"><?= $site->title() ?></h1>
+                <h1 class="header__title"><?= $site->heading()->html() ?></h1>
 
         </div>
         </header>

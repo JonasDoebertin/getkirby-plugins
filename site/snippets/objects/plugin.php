22ce8152bdaa7a18e6c98a9ead5980e2e7e84b4d
Jonas Döbertin <jonas@doebertin.de>
2014-12-23 09:34:09 +0100

    SEO improvements

--- site/snippets/objects/plugin.php
+++ site/snippets/objects/plugin.php
@@ -1,15 +1,21 @@
 
 <article class="[ plugin  js-plugin ]<?= CssClasses::pluginAttributes($plugin) ?>">
     <i class="plugin__icon  icon--<?= $plugin->icon()->html() ?>"></i>
-    <h2 class="plugin__title"><?= $plugin->title()->html() ?></h2>
-    <strong class="plugin__subtitle"><?= $plugin->subtitle()->html() ?></strong>
-    <p class="plugin__description  js-plugin-description"><?= $plugin->description()->html() ?></p>
+    <h2 class="plugin__title">
+        <?= $plugin->title()->html() ?>
+    </h2>
+    <strong class="plugin__subtitle">
+        <?= $plugin->subtitle()->html() ?>
+    </strong>
+    <p class="plugin__description  js-plugin-description">
+        <?= $plugin->description()->html() ?>
+    </p>
     <ul class="plugin__links  list-inline">
         <li>
-            <a href="<?= $plugin->authorlink()->html() ?>" rel="author">By <?= $plugin->authorname()->html() ?></a>
+            <a href="<?= $plugin->authorlink()->html() ?>" rel="author nofollow">By <?= $plugin->authorname()->html() ?></a>
         <!--</li>-->
         <li>
-            <a href="<?= $plugin->downloadlink()->html() ?>"><?= $plugin->downloadtitle()->html() ?></a>
+            <a href="<?= $plugin->downloadlink()->html() ?>" rel="nofollow"><?= $plugin->downloadtitle()->html() ?></a>
         <!--</li>-->
     </ul>
 </article>

967cccc2472ff8a294f70804717770438d3d7416
Jonas Döbertin <hello@jd-powered.net>
2015-01-26 21:21:03 +0100

    Updated icon font

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -23,7 +23,7 @@
 
         <!-- styles -->
         <?= css('//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600') ?>
-        <?= css('/assets/css/main.@@1422302756196.css') ?>
+        <?= css('/assets/css/main.@@1422303620632.css') ?>
 
         <!-- favicons -->
         <link rel="apple-touch-icon" sizes="57x57" href="<?= url('/assets/images/favicons/apple-touch-icon-57x57.png') ?>">

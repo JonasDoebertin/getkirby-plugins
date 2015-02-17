a76fe60f692a28449ec5f19c87604d4da65c08a0
Jonas Döbertin <jonas@doebertin.de>
2015-02-17 11:21:02 +0100

    More icons

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -23,7 +23,7 @@
 
         <!-- styles -->
         <?= css('//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600') ?>
-        <?= css('/assets/css/main.@@1422355690074.css') ?>
+        <?= css('/assets/css/main.@@1424168445651.css') ?>
 
         <!-- favicons -->
         <link rel="apple-touch-icon" sizes="57x57" href="<?= url('/assets/images/favicons/apple-touch-icon-57x57.png') ?>">

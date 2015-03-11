35517c1255bf8baf8f94e06a73402136daad6128
Jonas Döbertin <hello@jd-powered.net>
2015-03-11 12:39:34 +0100

    Add RSS feed

---
+++ site/plugins/feed/template.php
@@ -0,0 +1,25 @@
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
+
+    <channel>
+        <title><?= xml($title) ?></title>
+        <link><?= xml($link) ?></link>
+        <lastBuildDate><?= date('r', $modified) ?></lastBuildDate>
+        <atom:link href="<?php echo xml($url) ?>" rel="self" type="application/rss+xml" />
+        <description><?= xml($site->description()->html()) ?></description>
+
+        <?php foreach($items as $item): ?>
+            <item>
+                <title><?= xml($item->title()) ?></title>
+                <link><?= xml(url('/') . '#' . $item->uid()) ?></link>
+                <guid><?= xml($item->uid()) ?></guid>
+                <pubDate><?= $item->date('r', 'created') ?></pubDate>
+                <description>
+                    <![CDATA[
+                        <?= $item->description()->kirbytext() ?>
+                    ]]>
+                </description>
+            </item>
+        <?php endforeach ?>
+
+  </channel>
+</rss>

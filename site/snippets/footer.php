c3212de33894b5d0bb4c9a787d0f28063dfae38d
Jonas Döbertin <hello@jd-powered.net>
2015-06-26 10:02:01 +0200

    Bumped jQuery version number

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -9,10 +9,10 @@
         </footer>
 
 
-        <?= js('//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js') ?>
-        <script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.1.min.js"><\/script>')</script>
-        <?= js('assets/js/plugins.@@1435262289089.min.js') ?>
-        <?= js('assets/js/main.@@1435262289089.min.js') ?>
+        <?= js('//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js') ?>
+        <script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.3.min.js"><\/script>')</script>
+        <?= js('assets/js/plugins.@@1435305363238.min.js') ?>
+        <?= js('assets/js/main.@@1435305363238.min.js') ?>
 
         <?php snippet('tools/tracking') ?>
 
